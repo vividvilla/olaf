cff5d488d3d9c29e2e13fcaa0f26f2fe21fde30d
Vivek R <vividvilla@gmail.com>
2015-01-08 00:55:31 +0530

    Added page and posts view

--- make.py
+++ make.py
@@ -1,23 +1,38 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, abort
 from flask_flatpages import FlatPages
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 #App module initialize
-pages = FlatPages(app)
+contents = FlatPages(app)
 
 #Routes
 @app.route('/')
 def index():
 	return 'Hello World !!! This is my superblog'
 
-@app.route('/<path:path>/')
-def page(path):
+@app.route('/<slug>/')
+def posts(slug):
+	#Check for slug in root which means pages
+	content = [page for page in contents if page.path == slug]
+
+	#Check for slug in posts if not found in pages
+	if not content:
+		content = [post for post in contents
+					if post.path[11::] == slug]
+
+	if not content:
+		abort(404) #Slug not found both in pages and posts
+
+	#Exception if duplicates found
+	if len(content) > 1:
+		raise Exception('Duplicate slug')
+
+	print content[0].html
 	return render_template('page.html',
-		page=pages.get_or_404(path),
-		site=app.config.get('SITE'))
+				page=content[0], site=app.config.get('SITE'))
 
 if __name__ == '__main__':
 	app.run()
--- pages/hello.md
+++
@@ -1,3 +0,0 @@
-title: 'Hello world !!!'
-
-This is my first blog post.
