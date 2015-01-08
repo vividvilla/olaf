64d23f2212ad06f54b5b132aa474b83c75c1eda9
Vivek R <vividvilla@gmail.com>
2015-01-08 23:02:12 +0530

    Added filter function to get posts and replaced views logi with filter function and added counter to tags page

--- make.py
+++ make.py
@@ -1,6 +1,9 @@
+from collections import Counter
+
 from flask import Flask
 from flask import render_template, abort
 from flask_flatpages import FlatPages
+
 import config
 
 app = Flask(__name__)
@@ -9,35 +12,67 @@ app.config.from_object('config')
 #App module initialize
 contents = FlatPages(app)
 
+def get_posts(**filters):
+	"""
+	Filters posts
+	All arguments are optional
+
+	Optional keyword arguments
+
+	'sort'		: boolean	:: sort by timestamp (default: True)
+	'reverse'	: boolean	:: Reverse the order (default: True)
+	'tag'		: string	:: Filters by tag name (default: None)
+	'page_no'	: integer	:: Paginates results and returns result
+								according to page_no (default: None)
+	'limit'		: integer	:: Pagination limit (default: global limit)
+	'abort'		: boolean	:: Aborts with 404 error if no posts in
+								filtered results (default: False)
+	"""
+	posts = [post for post in contents
+				if post.meta.get('type') == 'post']
+
+	#Filter conditions
+
+	#Sort posts by timestamp (True by default)
+	sort = filters.get('sort', True)
+	if(sort):
+		posts.sort(key = lambda x: x.meta['timestamp'],
+					reverse = filters.get('reverse', True))
+
+	#Filter based on tag
+	tag = filters.get('tag')
+	if(tag):
+		posts = [post for post in posts
+					if tag in post.meta.get('tags', [])]
+
+	#Filter based on page number and pagination limit
+	page_no = filters.get('page_no')
+	limit = filters.get('limit') or config.SITE['limit']
+	if(page_no):
+		paginated = [posts[n:n+limit] for n in range(0, len(posts), limit)]
+		try:
+			posts = paginated[page_no - 1]
+		except IndexError:
+			posts = []
+
+	#Abort if posts not found
+	if(not posts and filters.get('abort') == True):
+		abort(404)
+
+	return posts
+
 #Routes
 
 #Home page
 @app.route('/')
 def index():
-	#Get posts and sort by timestamp
-	posts = sorted([post for post in contents
-				if post.meta.get('type', '') == 'post'], reverse = True,
-					key = lambda x: x.meta.get('timestamp', 0))
-
-	#Return posts as per settings 'limit'
-	return render_template('index.html', posts=posts[:config.SITE['limit']])
+	return render_template('index.html', posts=get_posts(page_no = 1))
 
 #For pagination
 @app.route('/<int:page_no>/')
 def pagination(page_no):
-	limit = config.SITE['limit']
-
-	posts = sorted([post for post in contents
-				if post.meta.get('type') == 'post'], reverse = True,
-					key = lambda x: x.meta['timestamp'])
-	paginated = [posts[n:n+limit] for n in range(0, len(posts), limit)]
-
-	try:
-		filtered_posts = paginated[page_no]
-	except IndexError:
-		abort(404)
-
-	return render_template('index.html', posts=filtered_posts)
+	return render_template('index.html',
+			posts=get_posts(page_no = page_no), abort = True)
 
 @app.route('/<path:slug>/')
 def posts(slug):
@@ -58,20 +93,23 @@ def posts(slug):
 
 	return render_template('page.html', page=content[0])
 
-@app.route('/tags/<string:tag>/')
-def tag(tag):
-	posts = [post for post in contents
-				if tag in post.meta.get('tags', [])]
-
-	return render_template('tag.html', tag = tag, posts=posts)
-
 @app.route('/tags/')
 def tags():
 	tags = [tag for post in contents
 				for tag in post.meta.get('tags', [])]
-	tags = list(set(tags))
-
+	tags = dict(Counter(tags)) #Count tag occurances
+	print tags
 	return render_template('tags.html', tags = tags)
+
+@app.route('/tags/<string:tag>/')
+def tag(tag):
+	return render_template('tag.html', tag=tag,
+		posts=get_posts(tag=tag, page_no=1, abort=True))
+
+@app.route('/tags/<string:tag>/<int:page_no>/')
+def tag_pages(tag, page_no):
+	return render_template('tag.html', tag=tag, page_no=page_no,
+			posts=get_posts(tag=tag, page_no=page_no, abort=True))
 
 if __name__ == '__main__':
 	app.run()
