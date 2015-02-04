08a4755d1643bdb88641d0cc427be4705e8b75d0
Vivek <vividvilla@gmail.com>
2015-02-04 17:33:31 +0530

    ran flake and corrected pep8 styles

--- olaf.py
+++ olaf.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-    Olaf
-    ~~~~~~~~~
+	Olaf
+	~~~~~~~~~
 
-    Main utility to run and freeze blog
+	Main utility to run and freeze blog
 
-    :copyright: (c) 2015 by Vivek R.
-    :license: BSD, see LICENSE for more details.
+	:copyright: (c) 2015 by Vivek R.
+	:license: BSD, see LICENSE for more details.
 """
 
 import sys
@@ -19,12 +19,12 @@ from flask import Flask
 from urlparse import urljoin
 from flask_frozen import Freezer
 from werkzeug.contrib.atom import AtomFeed
-from flask import render_template, abort, redirect, \
-		url_for, request, make_response
+from flask import render_template, abort, redirect, url_for, \
+	request, make_response
 from flask_flatpages import FlatPages, pygments_style_defs
 
 from utils import timestamp_tostring, date_tostring, \
-		font_size, console_message
+	font_size, console_message
 
 # If config file not found copy sample config
 if not os.path.exists('config.py'):
@@ -36,14 +36,16 @@ import config
 app = Flask(__name__)
 app.config.from_object('config')
 
-#App module initialize
+# App module initialize
 contents = FlatPages(app)
 freeze = Freezer(app)
-exclude_from_sitemap = [] #List of urls to be excluded from XML sitemap
+exclude_from_sitemap = []  # List of urls to be excluded from XML sitemap
 
-#Register utility functions to be used in jinja2 templates
-app.jinja_env.globals.update(timestamp_tostring=timestamp_tostring,
-			date_tostring=date_tostring, font_size=font_size)
+# Register utility functions to be used in jinja2 templates
+app.jinja_env.globals.update(
+	timestamp_tostring=timestamp_tostring,
+	date_tostring=date_tostring, font_size=font_size)
+
 
 def get_posts(**filters):
 	"""
@@ -62,45 +64,44 @@ def get_posts(**filters):
 								filtered results (default: False)
 	"""
 
-	posts = [post for post in contents
-				if post.meta.get('type') == 'post']
+	posts = [post for post in contents if post.meta.get('type') == 'post']
 
-	#Filter conditions
+	# Filter conditions
 
-	#Sort posts by timestamp (True by default)
+	# Sort posts by timestamp (True by default)
 	sort = filters.get('sort', True)
 	if(sort):
-		posts.sort(key = lambda x: x.meta['timestamp'],
-					reverse = filters.get('reverse', True))
+		posts.sort(
+			key=lambda x: x.meta['timestamp'],
+			reverse=filters.get('reverse', True))
 
-	#Filter based on tag
+	# Filter based on tag
 	tag = filters.get('tag')
 	if(tag):
-		posts = [post for post in posts
-					if tag in post.meta.get('tags', [])]
+		posts = [post for post in posts if tag in post.meta.get('tags', [])]
 
-	#Filter based on year and month
+	# Filter based on year and month
 	year = filters.get('year')
 	if(year):
 		posts = [post for post in posts if int(timestamp_tostring(
-						post.meta.get('timestamp'), '%Y')) == year]
+			post.meta.get('timestamp'), '%Y')) == year]
 
-		#Filter by month only if year is given
+		# Filter by month only if year is given
 		month = filters.get('month')
 		if month:
 			posts = [post for post in posts if int(timestamp_tostring(
-							post.meta.get('timestamp'), '%m')) == month]
+				post.meta.get('timestamp'), '%m')) == month]
 
 	# Get total number of posts without pagination
 	post_meta = {}
 	post_meta['total_pages'] = len(posts)
 
-	#Filter based on page number and pagination limit
+	# Filter based on page number and pagination limit
 	page_no = filters.get('page_no')
 	limit = filters.get('limit') or config.SITE['limit']
-	max_pages = 0;
+	max_pages = 0
 	if(page_no):
-		paginated = [posts[n:n+limit] for n in range(0, len(posts), limit)]
+		paginated = [posts[n:n + limit] for n in range(0, len(posts), limit)]
 		max_pages = len(paginated)
 		try:
 			posts = paginated[page_no - 1]
@@ -109,41 +110,40 @@ def get_posts(**filters):
 
 	post_meta['max_pages'] = max_pages
 
-	#Abort if posts not found
-	if(not posts and filters.get('abort') == True):
+	# Abort if posts not found
+	if not posts and filters.get('abort') is True:
 		abort(404)
 
 	return (posts, post_meta)
 
+
 def get_posts_by_slug(slug):
-	#Check for slug in root which means pages
+	# Check for slug in root which means pages
 	content = [page for page in contents if page.path == slug]
 
-	#Check for slug in posts if not found in pages
+	# Check for slug in posts if not found in pages
 	if not content:
-		content = [post for post in contents
-					if post.path[11::] == slug]
+		content = [post for post in contents if post.path[11::] == slug]
 	return content
 
 
-
 """ Views """
+
 
 @app.route('/404.html')
 def custom_404():
 	""" Custom 404 page view """
 	return render_template('404.html')
 
+
 @app.route('/pygments.css')
 def pygments_css():
 	""" Default pygments style """
 	pyments_style = config.SITE.get('pygments') or 'tango'
-	return (pygments_style_defs(pyments_style),
-				200, {'Content-Type': 'text/css'})
+	return (pygments_style_defs(pyments_style), 200, {'Content-Type': 'text/css'})
 
-exclude_from_sitemap.append('/pygments.css') # Excludes url from sitemap
+exclude_from_sitemap.append('/pygments.css')  # Excludes url from sitemap
 
-# Index vidw
 
 def get_index():
 	""" Check if custom home page set else return default index view """
@@ -162,11 +162,13 @@ def get_index():
 	else:
 		return default_index
 
+
 def default_index():
 	""" Default index view """
-	posts, post_meta = get_posts(page_no = 1)
-	return render_template('index.html', page_no = 1,
-		posts = posts, next_page = (post_meta['max_pages'] > 1))
+	posts, post_meta = get_posts(page_no=1)
+	return render_template('index.html',
+		page_no=1, posts=posts, next_page=post_meta['max_pages'] > 1)
+
 
 def custom_index():
 	""" Custom home page view """
@@ -177,6 +179,7 @@ def custom_index():
 # Set home page
 app.add_url_rule('/', 'index', get_index())
 
+
 @app.route('/pages/<int:page_no>/')
 def pagination(page_no):
 	""" Home page pagination view """
@@ -185,10 +188,11 @@ def pagination(page_no):
 	if page_no == 1 and not config.SITE.get('custom_home_page'):
 		return redirect(url_for('index'))
 
-	posts, post_meta = get_posts(page_no = page_no, abort = True)
-	return render_template('index.html', page_no = page_no,
+	posts, post_meta = get_posts(page_no=page_no, abort=True)
+	return render_template('index.html', page_no=page_no,
 			posts=posts, next_page=(post_meta['max_pages'] > page_no),
 			previous_page=(page_no > 1))
+
 
 @app.route('/<path:slug>/')
 def posts(slug):
@@ -196,9 +200,9 @@ def posts(slug):
 	content = get_posts_by_slug(slug)
 
 	if not content:
-		abort(404) #Slug not found both in pages and posts
+		abort(404)  # Slug not found both in pages and posts
 
-	#Exception if duplicates found
+	# Exception if duplicates found
 	if len(content) > 1:
 		raise Exception('Duplicate slug')
 
@@ -212,11 +216,12 @@ def tags():
 	""" List of tags view """
 	tags = [tag for post in contents
 				for tag in post.meta.get('tags', [])]
-	tags = sorted(Counter(tags).items()) #Count tag occurances
+	tags = sorted(Counter(tags).items())  # Count tag occurances
 
 	max_occ = 0 if not tags else max([tag[1] for tag in tags])
 
-	return render_template('tags.html', tags = tags, max_occ = max_occ)
+	return render_template('tags.html', tags=tags, max_occ=max_occ)
+
 
 @app.route('/tags/<string:tag>/')
 def tag_page(tag):
@@ -225,6 +230,7 @@ def tag_page(tag):
 	return render_template('tag.html', tag=tag, posts=posts,
 			page_no=1, next_page=(post_meta['max_pages'] > 1),
 			len = post_meta['total_pages'])
+
 
 @app.route('/tags/<string:tag>/pages/<int:page_no>/')
 def tag_pages(tag, page_no):
@@ -239,12 +245,12 @@ def tag_pages(tag, page_no):
 			page_no=page_no, next_page=(post_meta['max_pages'] > page_no),
 			len = post_meta['total_pages'], previous_page=(page_no > 1))
 
-# Archive views
 
 @app.route('/list/posts/')
 def list_posts():
 	""" All posts list view """
-	return render_template("list_posts.html", posts = get_posts()[0])
+	return render_template("list_posts.html", posts=get_posts()[0])
+
 
 @app.route('/list/pages/')
 def list_pages():
@@ -252,7 +258,8 @@ def list_pages():
 	pages = [page for page in contents
 				if page.meta.get('type') == 'page']
 
-	return render_template("list_pages.html", pages = pages)
+	return render_template("list_pages.html", pages=pages)
+
 
 @app.route('/archive/')
 def archive():
@@ -273,15 +280,18 @@ def archive():
 		year = i[0]
 		yearly_dict[year] = {}
 		yearly_dict[year]['count'] = i[1]
-		yearly_dict[year]['months'] = [(j[0][1], j[1]) for j in monthly if j[0][0] == year]
+		yearly_dict[year]['months'] = [(j[0][1], j[1])
+				for j in monthly if j[0][0] == year]
 
-	return render_template('archive.html', archive = yearly_dict)
+	return render_template('archive.html', archive=yearly_dict)
+
 
 @app.route('/archive/<int:year>/')
 def yearly_archive(year):
 	""" Yearly archive view """
 	posts, post_meta = get_posts(year=year, abort=True)
 	return render_template('archive_page.html', tag=year, year=year, posts=posts)
+
 
 @app.route('/archive/<int:year>/<int:month>/')
 def monthly_archive(year, month):
@@ -291,14 +301,12 @@ def monthly_archive(year, month):
 	return render_template('archive_page.html', tag=date_string, year=year,
 			month=month, posts=posts)
 
-# Ato, feed generator
 
 @app.route('/recent.atom')
 def recent_feed():
 	""" Atom feed generator """
 	feed = AtomFeed('Recent Articles',
 					feed_url=request.url, url=request.url_root)
-
 	posts, max_page = get_posts()
 	feed_limit = config.SITE.get('feed_limit', 10)
 
@@ -317,22 +325,20 @@ def recent_feed():
 
 	return feed.get_response()
 
-# XML sitemap
 
-# a route for generating sitemap.xml
 @app.route('/sitemap.xml', methods=['GET'])
 def sitemap():
 	""" XML sitemap generator """
-	pages=[]
+	pages = []
 
-	#Set last updated date for static pages as 10 days before
-	ten_days_ago= (datetime.datetime.now() -
-				datetime.timedelta(days=10)).date().isoformat()
+	# Set last updated date for static pages as 10 days before
+	ten_days_ago = (datetime.datetime.now() -
+		datetime.timedelta(days=10)).date().isoformat()
 
 	# Add static pages
 	for rule in app.url_map.iter_rules():
-		if ("GET" in rule.methods and len(rule.arguments) == 0
-				and rule.rule not in exclude_from_sitemap):
+		if ("GET" in rule.methods and len(rule.arguments) == 0 and
+			rule.rule not in exclude_from_sitemap):
 			pages.append([rule.rule, ten_days_ago])
 
 	# Add posts
@@ -342,10 +348,10 @@ def sitemap():
 		# Get post updation or creation date
 		if post.meta.get('updated'):
 			updated = datetime.datetime.fromtimestamp(
-					int(post.meta['updated'])).date().isoformat()
+				int(post.meta['updated'])).date().isoformat()
 		else:
 			updated = datetime.datetime.fromtimestamp(
-					int(post.meta['timestamp'])).date().isoformat()
+				int(post.meta['timestamp'])).date().isoformat()
 		# Add posts url
 		pages.append([post.path[10::], updated])
 
@@ -359,14 +365,16 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		description='Olaf - because some blogs are worth freezing for.')
 	parser.add_argument('-f', '--freeze', action='store_true', help='Freeze site')
-	parser.add_argument('-p', '--port', type=int, default=5000,
-			help='Port to run app [default: 5000]')
-	parser.add_argument('-host', '--host', type=str, default='127.0.0.1',
-			help='Port to run app [default: 5000]')
+	parser.add_argument(
+		'-p', '--port', type=int, default=5000,
+		help='Port to run app [default: 5000]')
+	parser.add_argument(
+		'-host', '--host', type=str, default='127.0.0.1',
+		help='Port to run app [default: 5000]')
 	args = parser.parse_args()
 
 	if args.freeze:
-		console_message('Successfully freezed.', 'OKGREEN', newline = True)
+		console_message('Successfully freezed.', 'OKGREEN', newline=True)
 		freeze.freeze()
 	else:
-		app.run(port = args.port, host = args.host)
+		app.run(port=args.port, host=args.host)
